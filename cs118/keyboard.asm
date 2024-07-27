192d81edcbc5530e2c3ada8960e15104d13c5263
Michael Gobaco <michael.gobaco@plutotv.com>
2024-07-27 10:32:11 +0000

    pp12 issues

--- cs118/keyboard.asm
+++ cs118/keyboard.asm
@@ -8,16 +8,16 @@ _start:
     movl $0, numvalue     # Initialize numvalue to 0
 
 loop:
-    movl $0, %eax
+    movl $0, %ecx
     call getchar          # Get next character
-    cmpl $10, %eax        # Compare with newline (ASCII 10)
+    cmpl $10, %ecx        # Compare with newline (ASCII 10)
     je done               # If newline, we're done
 
-    subl $48, %eax        # Convert ASCII to digit
+    subl $48, %ecx        # Convert ASCII to digit
 
     movl numvalue, %ebx   # Load current numvalue
     imull $10, %ebx       # Multiply numvalue by 10
-    addl %eax, %ebx       # Add new digit
+    addl %ecx, %ebx       # Add new digit
 
     movl %ebx, numvalue   # Store result back in numvalue
 
@@ -30,12 +30,24 @@ done:
     int $0x80             # Make system call
 
 getchar:
-    # Read a single character from stdin
+    # Prologue
+    pushl %ebp
+    movl %esp, %ebp
     pushl %ebx
+    pushl %esi
+
+    # Read a single character from stdin
     movl $3, %eax         # sys_read system call
     movl $0, %ebx         # File descriptor (0 for stdin)
     movl %esp, %ecx       # Buffer to store the character
-    movl $1, %edx         # Read 1 byte
+    movl $1, %edx         # Read 1   bytes
     int $0x80
-    popl %eax
+    popl %ecx
+
+    # Epilogue
+    popl %esi
+    popl %ebx
+    movl %ebp, %esp
+    popl %ebp
+
     ret
