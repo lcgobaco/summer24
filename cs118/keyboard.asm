73b5bf5327a25489c2a9cbe7ec03a18151f72d4c
Michael Gobaco <michael.gobaco@plutotv.com>
2024-07-27 12:12:04 +0000

    keyboard

--- cs118/keyboard.asm
+++ cs118/keyboard.asm
@@ -1,5 +1,6 @@
 .section .data
     numvalue: .long 0
+    buffer: .space 1
 
 .section .text
 .globl _start
@@ -8,46 +9,47 @@ _start:
     movl $0, numvalue     # Initialize numvalue to 0
 
 loop:
-    movl $0, %ecx
+    mov $0, %eax
     call getchar          # Get next character
-    cmpl $10, %ecx        # Compare with newline (ASCII 10)
+    cmp $10, %eax        # Compare with newline (ASCII 10)
     je done               # If newline, we're done
 
-    subl $48, %ecx        # Convert ASCII to digit
+    sub $48, %eax        # Convert ASCII to digit
 
-    movl numvalue, %ebx   # Load current numvalue
-    imull $10, %ebx       # Multiply numvalue by 10
-    addl %ecx, %ebx       # Add new digit
+    mov numvalue, %ebx   # Load current numvalue
+    imul $10, %ebx       # Multiply numvalue by 10
+    add %eax, %ebx       # Add new digit
 
-    movl %ebx, numvalue   # Store result back in numvalue
+    mov %ebx, numvalue   # Store result back in numvalue
 
     jmp loop              # Continue loop
 
 done:
     # Exit program (you might want to print the result instead)
-    movl $1, %eax         # sys_exit system call
-    xorl %ebx, %ebx       # Exit code 0
+    mov $1, %eax         # sys_exit system call
+    xor %ebx, %ebx       # Exit code 0
     int $0x80             # Make system call
 
 getchar:
     # Prologue
-    pushl %ebp
-    movl %esp, %ebp
-    pushl %ebx
-    pushl %esi
+    push %ebp
+    mov %esp, %ebp
+    push %ebx
+    push %esi
 
     # Read a single character from stdin
-    movl $3, %eax         # sys_read system call
-    movl $0, %ebx         # File descriptor (0 for stdin)
-    movl %esp, %ecx       # Buffer to store the character
-    movl $1, %edx         # Read 1   bytes
+    mov $3, %eax         # sys_read system call
+    mov $0, %ebx         # File descriptor (0 for stdin)
+    leal buffer, %ecx      # Buffer to store the character
+    mov $1, %edx         # Read 1 bytes
     int $0x80
-    popl %ecx
+
+    mov buffer, %eax
 
     # Epilogue
-    popl %esi
-    popl %ebx
-    movl %ebp, %esp
-    popl %ebp
+    pop %esi
+    pop %ebx
+    mov %ebp, %esp
+    pop %ebp
 
     ret
