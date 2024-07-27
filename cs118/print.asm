4871049a6eacf9a487f1d587cec7d353b8619c4a
Lucas Gobaco <lucas@rn01-gobaco-com.tail354d1.ts.net>
2024-07-27 01:09:35 +0000

    pp10 print

--- cs118/print.asm
+++ cs118/print.asm
@@ -1,26 +1,38 @@
-.section .data
-    prompt: .ascii "Hello, Assembly World!\n"
-    promptSiz = . - prompt
+# Name: Lucas Gobaco
+# Date: 26 July 2024
+# Program Name: Programming Project 10
+# Program Description: This program takes a string and prints it out in the terminal.
 
-.section .text
-    .globl _start
+# Register Use List:
+# eax: stores constant for write
+# ebx: stores constant for stdout
+# ecx: stores address of output
+# edx: stores number of characters to write
+# esi: stores counter for number of digits
 
-_start:
-    # Print the message
+.data
+    prompt: .ascii "Hello, Assembly World!\n"   # creates string using .ascii
+    promptSiz = . - prompt                      # '.' represents the current address, so ". - prompt" calculates the difference between the two addresses and saves it into promptSiz
+ 
+    # symbol table
+    .equ    STDIN, 0
+    .equ    STDOUT, 1
+    .equ    READ, 3
+    .equ    WRITE, 4
+    .equ    EXIT, 1
+    .equ    SUCCESS, 0
+
+.text
+
+.globl _start
+_start: # prints the prompt
     movl    $promptSiz, %edx
     movl    $prompt, %ecx
     movl    $STDOUT, %ebx
     movl    $WRITE, %eax
     int     $0x80
 
-    # Exit the program
+done:   # exits the program
     movl    $SUCCESS, %ebx
     movl    $EXIT, %eax
     int     $0x80
-
-.equ    STDIN, 0
-.equ    STDOUT, 1
-.equ    READ, 3
-.equ    WRITE, 4
-.equ    EXIT, 1
-.equ    SUCCESS, 0
