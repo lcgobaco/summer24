beeb6a9b990d4dd6a23abfe09238f43acab643bb
Michael Gobaco <michael.gobaco@plutotv.com>
2024-07-30 19:54:50 +0000

    math

--- cs118/math.asm
+++ cs118/math.asm
@@ -11,15 +11,17 @@
 # edx: stores maximum number of characters to read
 
 .data
-    numvalue: .long 0       # stores resulting integer
-    buffer: .space 1        # stores current keystroke
-    int1: .long 0           # first integer
-    int2: .long 0           # second integer
+    numvalue:   .long 0         # stores resulting integer
+    buffer:     .byte     # stores current keystroke
+    msg_digit:  .ascii "0"      # add to number to convert it into ascii equivalent
 
     # symbol table
     .equ    STDIN,0
+    .equ    STDOUT,1
     .equ    READ,3
+    .equ    WRITE,4
     .equ    EXIT,1
+    .equ    SUCCESS,0
     .equ    RETURN, 10
 
 .text
@@ -39,6 +41,9 @@ _start:
     add numvalue, %ebx  # Add the second number to it
 
     # The sum is now in %ebx
+    # print out the sum
+    mov %ebx, %eax
+    call print_ascii
 
 done:
     # gracefully exit out of program
@@ -102,3 +107,58 @@ getchar:
     pop %ebp
     ret
 
+print_ascii:
+    # saves current state of stack
+    push %ebp
+    mov %esp, %ebp
+    push %ebx
+    push %esi
+    xor %esi, %esi          # zero out esi to use as counter
+
+next_digit:
+    xor %edx, %edx          # zero out edx
+    mov $10, %ebx           # move 10
+    div %ebx                # divide eax by ebx
+    add $'0', %edx          # add '0' in ASCII to offset edx
+    push %edx               # take remainder and push into stack
+    inc %esi                # increment counter
+    cmp $0, %eax
+    jne next_digit          # loop if eax is not zero
+
+print_digits:
+    cmp $0, %esi
+    je print_newline        # print a new line if counter is zero
+
+    pop %edx                # remove top item from stack
+    mov %edx, msg_digit     # copy edx to msg_digit to print out
+
+    # print out msg_digit
+    push %eax               # save eax first as it will be overwritte
+    mov $WRITE, %eax
+    mov $STDOUT, %ebx
+    mov $msg_digit, %ecx
+    mov $1, %edx
+    int $0x80
+    pop %eax                # restore eax
+
+    dec %esi                # decrement counter
+    jmp print_digits        # loop until counter is zero
+
+print_newline:              # prints new line
+    push %eax               # save eax first
+    mov $WRITE, %eax
+    mov $STDOUT, %ebx
+    mov $RETURN, %ecx
+    mov %ecx, msg_digit
+    mov $msg_digit, %ecx
+    mov $1, %edx
+    int $0x80
+
+    pop %eax                # restore eax
+
+    # restores stack to original state before subroutine was called
+    pop %esi
+    pop %ebx
+    mov %ebp, %esp
+    pop %ebp
+    ret
